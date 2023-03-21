a10a5c6b07604d23c021c526aea9e92de6001808
SN0WYPEAKS <124600079+SN0WYPEAKS@users.noreply.github.com>
2023-03-21 13:45:59 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 //if search bar is in focus, show the search button
 //navbar-bar-toggle ID
 //search-button ID
-var bar = document.getElementById('navbar-bar-toggle');
-var search = document.getElementById('search-button');
+const bar = document.getElementById('navbar-bar-toggle');
+const search = document.getElementById('search-button');
+const productFilter = document.getElementById('product-filter');
+const filter = document.getElementById('filter-button');
+const closeFilter = document.getElementById('close-filter');
+const body = document.getElementById('body');
+const action = document.getElementById('action-button');
 
 search.style.display = 'none';
 
@@ -12,4 +17,21 @@ function searchFunc () {
 
 function hideButton () {
   search.style.display = 'none';
+}
+
+//inventory search function
+function invSearch () {}
+
+//filter moves page when open
+function filterMove () {
+  body.style.marginLeft = '300px';
+  filter.style.display = 'none';
+  productFilter.style.marginLeft = '-260px';
+}
+
+//moves page back when filter is closed
+function moveBack () {
+  body.style.marginLeft = '40px';
+  filter.style.display = 'block';
+  productFilter.style.marginLeft = '0px';
 }
